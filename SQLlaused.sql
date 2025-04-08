10243b12c3bc36ef19f5c2d0c4e523efb28f84aa
MarkShapiro123 <quintxdd@gmail.com>
2025-04-08 13:38:55 +0300

    xampp

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -1,3 +1,43 @@
+--Xampp control panel
+--Conect TO:
+--server name: localhost -127.0.0.1
+--Authentification:
+--kasutaja: root
+--paroot: ei ole
+CREATE DATABASE MarkShapiroLOGITpv23;
+USE MarkShapiroLOGITpv23;
+
+--tabeli loomine
+--identity(1,1) - ise täidab tabeli 1,2,3,...
+CREATE TABLE inimene(
+inimeneID int Primary Key identity(1,1),
+nimi varchar(50) unique,
+synniaeg date,
+telefon char(12),
+pikkus decimal(5,2),
+opilaskodu bit
+);
+SELECT * FROM inimene;
+
+--tabeli kustutamine
+DROP table inimene;
+SELECT * FROM inimene;
+
+--tabeli kustutamine
+DROP table inimene;
+
+--andmete lisamine
+--DDL - data definition language
+--DML -data manipulation language
+
+INSERT INTO inimene
+(nimi, synniaeg, telefon, pikkus, opilaskodu)
+VALUES
+('Peeter Uus', '2021-12-30', '2568952', 90.5, 0),
+('Kaarel Oja', '2000-12-30', '2568952', 90.5, 0),
+('Kask Mati', '1983-12-30', '33333', 140.5, 0),
+('Peeter Oja', '2021-12-30', '333333', 190.5, 0)
+;
 SELECT * FROM inimene;
 --tabel elukoht
 CREATE TABLE elukoht(
